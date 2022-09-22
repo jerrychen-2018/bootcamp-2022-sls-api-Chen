f26506f57b91c3041286b9898a844e5dcf194f66
Jerry Chen <jchen@dminc.com>
2022-09-22 14:35:12 -0400

    Hope API works now

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
 export GITHUB_USERNAME="jerrychen-2018"
-export GITHUB_REPO="bootcamp-2022-sls-api-Chen.git"
+export GITHUB_REPO="bootcamp-2022-sls-api-Chen"
 export BUCKET_NAME="jchendminc-bootcamp-2022-bucket" #username must be all lowercase
